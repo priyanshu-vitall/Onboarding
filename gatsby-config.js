236618f0044626c0786daaf1776ebf8234330be5
Priyanshu Bhatt <priyanshu-vitall@priyanshu-vitall.local>
2026-01-13 11:10:13 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Onboard`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
